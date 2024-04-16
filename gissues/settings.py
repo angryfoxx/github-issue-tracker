5cd2b527f1bf013d7525e22d7efd84faa8a65529
Ömer Faruk Korkmaz <omarfarukkorkmazz@gmail.com>
2024-04-17 01:19:55 +0300

    chore: Add django-stubs, mypy and some tools to write code easier and better.

--- gissues/settings.py
+++ gissues/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     # Third party apps
     "rest_framework",
     "django_filters",
+    "django_extensions",
     # Local apps
     "gissues",
     "gissues.extensions",
@@ -143,3 +144,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SHELL_PLUS = "ipython"
